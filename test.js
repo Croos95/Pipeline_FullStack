fd5fb7ae583e7199ead6a8009f05b8a597d40369
Jose Ruiz <joferuizca@ittepic.edu.mx>
2026-05-04 20:50:06 -0700

    xd

---
+++ test.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/api/products',
+  method: 'GET'
+};
+
+const req = http.request(options, res => {
+    if (res.statusCode === 400 || res.statusCode === 401 || res.statusCode === 403) {
+        console.log("Test Ok (La api se protege con autenticación)");
+        process.exit(0);
+    }else{
+        console.log("Test Fallo");
+        process.exit(1);
+    }
+});
+
+req.on('error', error => {
+  console.error(error);
+  process.exit(1);
+});
+
+req.end();
